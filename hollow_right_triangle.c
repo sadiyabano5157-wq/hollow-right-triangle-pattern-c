53cb2695644e56ba8460b4584897bd45975def27
sadiyabano5157-wq <sadiyabano5157@gmail.com>
2026-05-22 06:26:22 +0530

    Add hollow right triangle pattern program
    This program prints a hollow right triangle pattern based on user input for the number of rows.

---
+++ hollow_right_triangle.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter rows: ");
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            if(j==1 || j==i || i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
